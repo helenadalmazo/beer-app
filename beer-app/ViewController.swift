d19644ba33a8eb20d757100299f993d3ea96a754
Helena Dalmazo <helenadalmazo@outlook.com>
2020-08-13 21:13:38 -0300

    Setup single view app project

---
+++ beer-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beer-app
+//
+//  Created by Helena Dalmazo on 13/08/20.
+//  Copyright © 2020 Helena Dalmazo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
