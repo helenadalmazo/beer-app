e152c2c16349c6e44866391e6dd02757968559bd
Helena Dalmazo <helenadalmazo@outlook.com>
2020-09-03 21:47:22 -0300

    Display beer list on Table View

--- beer-app/Controller/BeerListTableViewController.swift
+++ beer-app/Controller/BeerListTableViewController.swift
@@ -12,7 +12,7 @@ class BeerListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -24,23 +24,23 @@ class BeerListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return BeerRepository.shared.beers.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "beerReuseIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = BeerRepository.shared.beers[indexPath.row].name
 
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
