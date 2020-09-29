7485b3146e174fe82b3f61f7b931c3cc7c929665
Helena Dalmazo <helenadalmazo@outlook.com>
2020-09-28 22:47:34 -0300

    Removing setupImages func, 'cause de images don't need be in memory

--- beer-app/View/BeerRatingView.swift
+++ beer-app/View/BeerRatingView.swift
@@ -11,9 +11,8 @@ import UIKit
 class BeerRatingView: UIStackView {
     
     private var buttons = [UIButton]()
-    private var images = [UIImage]()
     
-    private let COUNT = 5
+    private let count = 5
     
     var rating = 0 {
         didSet {
@@ -23,18 +22,16 @@ class BeerRatingView: UIStackView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupImages()
         setupButtons()
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
-        setupImages()
         setupButtons()
     }
     
     @objc func buttonTapped(button: UIButton) {
-        guard let index = buttons.index(of: button) else {
+        guard let index = buttons.firstIndex(of: button) else {
             return
         }
 
@@ -46,22 +43,17 @@ class BeerRatingView: UIStackView {
             rating = selectedRating
         }
     }
-    
-    private func setupImages() {
-        for index in 0..<COUNT {
-            let image = UIImage(named: "Beer Rating Images/\(index + 1)")!
-            images.append(image)
-        }
-    }
-    
+
     private func setupButtons() {
-        let level0 = UIImage(named: "Beer Rating Images/0")!
+        let emptyImage = UIImage(named: "Beer Rating Images/0")!
         
-        for index in 0..<COUNT {
+        for index in 0..<count {
             let button = UIButton()
             
-            button.setImage(level0, for: .normal)
-            button.setImage(images[index], for: .selected)
+            button.setImage(emptyImage, for: .normal)
+            
+            let imageForIndex = UIImage(named: "Beer Rating Images/\(index + 1)")!
+            button.setImage(imageForIndex, for: .selected)
         
             button.translatesAutoresizingMaskIntoConstraints = false
             button.heightAnchor.constraint(equalToConstant: 50).isActive = true
