81a0f4884334adf37bb8463a48246e3194126c8a
Helena Dalmazo <helenadalmazo@outlook.com>
2020-09-28 21:52:30 -0300

    Creating View for beer rating control

---
+++ beer-app/View/BeerRatingView.swift
@@ -0,0 +1,21 @@
+//
+//  BeerRatingView.swift
+//  beer-app
+//
+//  Created by Helena Dalmazo on 28/09/20.
+//  Copyright © 2020 Helena Dalmazo. All rights reserved.
+//
+
+import UIKit
+
+class BeerRatingView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
